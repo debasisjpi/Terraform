c21b361109cce675abb563bf063ad94d23ce4959
debasis <debasisjpi@outlook.com>
2025-08-22 22:04:51 +0530

    new file:   .gitignore 	new file:   .terraform.lock.hcl 	modified:   ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-1"
 }
 
-resource "aws_instace" "myec2" {
+resource "aws_instance" "myec2" {
     instance_type = "t2.micro"
-    ami = "ami0360c520857e3138f"  
+    ami = "ami-0360c520857e3138f"  
 }
